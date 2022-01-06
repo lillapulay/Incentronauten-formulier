207b52be7604f7ce1cf1d3d4a655365bf79fc60a
Lilla Pulay <pulaylilla@gmail.com>
2022-01-06 13:27:11 +0100

    Some input attributes, logging values

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const submitData = () => {
+  /* Targeting input fields and their values */
+  const initials = document.getElementById('initials').value;
+  const infix = document.getElementById('infix').value;
+  const lastname = document.getElementById('lastname').value;
+  const postcode = document.getElementById('postcode').value;
+  const number = document.getElementById('number').value;
+  const street = document.getElementById('street').value;
+  const city = document.getElementById('city').value;
+  const email = document.getElementById('email').value;
+
+  /* Logging values for testing */
+  console.log(
+    initials + infix + lastname + postcode + number + street + city + email
+  );
+};
